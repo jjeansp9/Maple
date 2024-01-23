8370e6359e2ccda4866144a6bd2e9bf425616fb6
jinsol <jjeansp9@gmail.com>
2024-01-23 23:44:28 +0900

    Initial commit

---
+++ app/src/main/java/com/jspstudio/maple/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jspstudio.maple
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jspstudio.maple.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initData()
+    }
+
+    private fun initData() {
+
+    }
+}
