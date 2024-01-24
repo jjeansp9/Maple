66abf3533039d5adda766dfe5025b3978eba72f1
jinsol <jjeansp9@gmail.com>
2024-01-24 21:43:27 +0900

    commit

--- app/src/main/java/com/jspstudio/maple/MainActivity.kt
+++ app/src/main/java/com/jspstudio/maple/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jspstudio.maple
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,14 +12,44 @@ import com.jspstudio.maple.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var dialog: NormalDialog? = null
+
+    private var price_10 = 0
+    private var price_60 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        initData()
+        onClick()
     }
 
-    private fun initData() {
+    private fun onClick() {
+        binding.tvSetGlove10.setOnClickListener {
+            showDialog(binding.tvGlove10Price)
+        }
+        binding.tvSetGlove60.setOnClickListener {
+            showDialog(binding.tvGlove60Price)
+        }
+    }
 
+    private fun showDialog(view: TextView) {
+        if (dialog == null || dialog?.isAdded == false) {
+            dialog = NormalDialog().apply {
+                setTitle("주문서의 금액을 설정해주세요")
+                okClick {
+                    view.text = it.toString() + " 메소"
+                    when(view) {
+                        binding.tvGlove10Price -> price_10 = it
+                        binding.tvGlove60Price -> price_60 = it
+                    }
+                    dismiss()
+                }
+                cancelClick { dismiss() }
+            }
+        }
+        // Dialog가 이미 추가되었는지 확인하고, 추가되지 않았다면 표시
+        if (!dialog!!.isAdded) {
+            dialog!!.show(this.supportFragmentManager, "normalDialog")
+        }
     }
 }
---
+++ app/src/main/java/com/jspstudio/maple/NormalDialog.kt
@@ -0,0 +1,56 @@
+package com.jspstudio.maple
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.jspstudio.maple.databinding.DialogNormalBinding
+
+class NormalDialog : DialogFragment() {
+
+    private val binding: DialogNormalBinding by lazy { DialogNormalBinding.inflate(layoutInflater) }
+
+    private var okClickListener: ((response: Int) -> Unit)? = null
+    private var cancelClickListener: View.OnClickListener? = null
+    private var title = ""
+    private var price = ""
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        return dialog
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+        initData()
+    }
+
+    private fun initData() {
+        binding.tvTitle.text = title
+    }
+
+    fun setTitle(str: String) { title = str }
+    fun setPrice(str: String) { price = str }
+    fun okClick(listener: ((response: Int) -> Unit)?) {
+        okClickListener = listener
+    }
+    fun cancelClick(listener: View.OnClickListener) { cancelClickListener = listener }
+
+    private fun onClick () {
+        binding.tvOk.setOnClickListener{
+            var num = 0
+
+            if (binding.etPrice.text.isNotEmpty()) num = binding.etPrice.text.toString().toInt()
+            if (okClickListener != null) okClickListener?.let { it1 -> it1(num) }
+        }
+
+        cancelClickListener?.let { binding.tvCancel.setOnClickListener(it) }
+    }
+}
